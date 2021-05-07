65b2d864714c1d912c0cc61908e6c34aefd41aa8
erick23r <79947181+erick23r@users.noreply.github.com>
2021-05-07 16:57:14 -0600

    Initial commit

---
+++ app/src/main/java/com/da39a/parcial2ds39a/MainActivity.java
@@ -0,0 +1,30 @@
+package com.da39a.parcial2ds39a;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mostrar = new Intent(MainActivity.this,Menu.class);
+                startActivity(mostrar);
+                finish();
+            }
+        }, 4300);
+    }
+}
